6b22a8bb431e3109b74223b86b822b52e756ceb2
Sy Le <lenguyensY@gmail.com>
2020-08-11 08:36:36 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Note Synchronizer
+================
+Synchronize Gmail Notes and will be pushed to Google Drive or as part of Google Keep Notes
---
+++ src/types.ts
@@ -0,0 +1,40 @@
+export interface Headers {
+  date: string;
+  subject: string;
+  from: string;
+  to: string;
+  bcc: string;
+  received: string;
+  "message-id": string;
+  "content-type": string;
+  "delivered-to": string;
+  "x-received": string;
+  "arc-seal": string;
+  "arc-message-signature": string;
+  "arc-authentication-results": string;
+  "return-path": string;
+  "received-spf": string;
+  "authentication-results": string;
+  "dkim-signature": string;
+  "x-google-dkim-signature": string;
+  "x-gm-message-state": string;
+  "x-google-smtp-source": string;
+  "mime-version": string;
+  [propName: string]: any;
+}
+
+export interface Attachment {
+  fileName: string;
+  attachmentId: string;
+  mimeType: string;
+  [propName: string]: any;
+}
+
+export interface Email {
+  id: string;
+  threadId: string;
+  body: string;
+  attachments: Attachment[];
+  headers: Headers;
+  date: number;
+}
