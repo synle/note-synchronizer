aeb0c591662ce53854bfbf7bf8bde6293a735533
Sy Le <lenguyensy@gmail.com>
2020-08-15 06:59:31 -0700

    add ignored tokens

--- process.ts
+++ process.ts
@@ -8,6 +8,7 @@ import {init as initGoogleApi, uploadFile} from './src/crawler/gmailCrawler';
 
 const myEmails = (process.env.MY_EMAIL || "").split("|||");
 const mySignatureTokens = (process.env.MY_SIGNATURE || "").split("|||");
+const ignoredTokens = (process.env.IGNORED_TOKEN || "").split("|||");
 
 const PROCESSED_EMAIL_PREFIX_PATH = "./processed";
 
@@ -29,7 +30,7 @@ async function _doWork() {
   });
 
   const totalMessageCount = matchedEmailsResponse.length;
-  console.log("Total Files To Process:", matchedEmailsResponse.length);
+  console.log("Total Files To Process:", totalMessageCount);
 
   let processedMessageCount = 0;
 
@@ -40,13 +41,13 @@ async function _doWork() {
       100
     ).toFixed(2);
     if (percentDone % 20 === 0 || processedMessageCount % 100 === 0) {
-      console.log(`> ${percentDone}%`);
+      console.log(`> ${percentDone}% (${processedMessageCount}/${totalMessageCount})`);
     }
 
 
     const email: Email = matchedEmailsResponse[i].dataValues;
 
-    const { threadId, id, body, from, bcc, to, subject } = email;
+    let { threadId, id, body, from, bcc, to, subject } = email;
     const toEmailList = (bcc || '').split(',').concat((to || '').split(','));
     const attachments : Attachment[] = email.Attachments.map(a => a.dataValues);
 
@@ -56,9 +57,20 @@ async function _doWork() {
         toEmailList.some((toEmail) => toEmail.includes(myEmail))
       )
     ) {
+      body = body.trim();
+      subject = subject.trim()
+
+      // if content contains the ignored patterns, then skip this
+      if (ignoredTokens.some(ignoredToken => body.includes(ignoredToken))) {
+        console.log('> Ignored: ', subject);
+        continue;
+      }
+
       const localPath = `${PROCESSED_EMAIL_PREFIX_PATH}/processed.${email.id}.data`;
 
-      fs.writeFileSync(localPath, `${subject}\n\n${body || ""}`.trim());
+      const fileContent = `${subject}\n${id}/${threadId}\n\n${body || ""}`;
+
+      fs.writeFileSync(localPath, fileContent.trim());
 
       // upload the doc itself
       const docFileName = subject;
