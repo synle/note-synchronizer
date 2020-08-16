9d843d20abea0a63af29045ddec04a4e302ae003
Sy Le <lenguyensy@gmail.com>
2020-08-16 08:14:21 -0700

    single job process

--- index.ts
+++ index.ts
@@ -3,11 +3,24 @@ require("dotenv").config();
 
 import initDatabase from "./src/models/modelsFactory";
 
-import gmailCrawler from "./src/crawler/gmailCrawler";
+import {
+  initGoogleApi,
+  doWorkForAllItems,
+  doWorkSingle,
+} from "./src/crawler/gmailCrawler";
 
 async function _doWork() {
   await initDatabase();
-  gmailCrawler();
+  await initGoogleApi();
+
+  const targetThreadId = process.argv[2]
+  if (targetThreadId){
+    console.log("Starting Gmail Crawler on a single item", targetThreadId);
+    doWorkSingle(targetThreadId);
+  }  else{
+    console.log('Starting Gmail Crawler on all items');
+    doWorkForAllItems();
+  }
 }
 
 _doWork();
