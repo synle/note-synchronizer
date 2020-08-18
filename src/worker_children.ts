c0ecf58a777cc067ba70d34683163039a15e5c46
Sy Le <lenguyensy@gmail.com>
2020-08-18 09:24:41 -0700

    convert the worker code to database...

--- src/worker_children.ts
+++ src/worker_children.ts
@@ -3,15 +3,44 @@ import { isMainThread } from "worker_threads";
 import { workerData } from "worker_threads";
 import { parentPort } from "worker_threads";
 
+import initDatabase from "./models/modelsFactory";
+
+import {
+  getNoteDestinationFolderId,
+  initGoogleApi,
+  uploadFile,
+} from "./crawler/googleApiUtils";
+
+import {
+  doGmailWorkPollThreadList,
+  doGmailWorkForAllItems,
+  doGmailWorkByThreadIds,
+  doDecodeBase64ForRawContent,
+} from "./crawler/gmailCrawler";
+
+import {
+  doGdriveWorkForAllItems,
+  doGdriveWorkByThreadIds,
+} from "./crawler/gdriveCrawler";
+
+import { logger } from "./loggers";
+
 if (isMainThread) {
   throw new Error("Its not a worker");
 }
 
-parentPort.on("message", (data: any) => {
-  console.log('child message do', data)
-  setTimeout(() => {
-    parentPort.postMessage("hello parents: " + data);
-  }, 3000);
-});
+async function _init(){
+  await initDatabase();
+  await initGoogleApi();
 
-console.log("child start", workerData);
+  parentPort.on("message", (data: any) => {
+    console.log("child message do", data);
+    setTimeout(() => {
+      parentPort.postMessage("hello parents: " + data);
+    }, 3000);
+  });
+
+  console.log("child started", workerData);
+}
+
+_init();
--- src/worker_parents.ts
+++ src/worker_parents.ts
@@ -2,6 +2,28 @@
 import path from "path";
 import { Worker } from "worker_threads";
 
+import initDatabase from "./models/modelsFactory";
+
+import {
+  getNoteDestinationFolderId,
+  initGoogleApi,
+  uploadFile,
+} from "./crawler/googleApiUtils";
+
+import {
+  doGmailWorkPollThreadList,
+  doGmailWorkForAllItems,
+  doGmailWorkByThreadIds,
+  doDecodeBase64ForRawContent,
+} from "./crawler/gmailCrawler";
+
+import {
+  doGdriveWorkForAllItems,
+  doGdriveWorkByThreadIds,
+} from "./crawler/gdriveCrawler";
+
+import { logger } from "./loggers";
+
 const workers = [];
 let maxThreadCount = 6;
 
@@ -16,9 +38,9 @@ function _newWorker(myThreadId) {
   const workerDetails = {};
 
   const worker = new Worker(path.join(__dirname, "worker_children.js"), {
-    workerData :{
+    workerData: {
       myThreadId,
-    }
+    },
   });
   worker.on("message", (data) => {
     console.log("parent received message from worker", myThreadId, data);
@@ -36,23 +58,28 @@ function _newWorker(myThreadId) {
   });
 
   workerDetails.work = worker;
-  workerDetails.status = 'FREE';
+  workerDetails.status = "FREE";
 
   return workerDetails;
 }
 
-while (maxThreadCount > 0) {
-  maxThreadCount--;
-  const myThreadId = workers.length;
-  workers.push(_newWorker(myThreadId));
+async function _init(){
+  while (maxThreadCount > 0) {
+    maxThreadCount--;
+    const myThreadId = workers.length;
+    workers.push(_newWorker(myThreadId));
+  }
+
+  console.log("parent started");
+
+  setInterval(() => {
+    for (let worker of workers) {
+      worker.status = WORKER_STATUS.BUSY;
+      worker.work.postMessage("message from parent " + Date.now());
+    }
+  }, 1000);
+
+
 }
 
-
-setInterval(() => {
-  for(let worker of workers){
-    worker.status = WORKER_STATUS.BUSY;
-    worker.work.postMessage("message from parent " + Date.now());
-  }
-}, 1000);
-
-console.log('parent start')
+_init();
