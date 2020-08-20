68412c9eeed5a8632ce49cb3b068bfbfee6781e4
Sy Le <lenguyensy@gmail.com>
2020-08-20 12:46:48 -0700

    for scale, break up databases for sqlite into multiple files

--- src/models/modelsFactory.ts
+++ src/models/modelsFactory.ts
@@ -3,36 +3,54 @@ import { Sequelize } from "sequelize";
 import { initDatabase } from "sequelize-typescript-decorators";
 import Models from "./modelsSchema";
 
-export let sequelize;
-
 /**
  * this routine will initialize the database, please only run this once per all...
  */
 export default async () => {
   // notes such as emails and attachments
-  sequelize = new Sequelize(
-    process.env.DB_NAME || "note_synchronize",
-    process.env.DB_USERNAME || "root",
-    process.env.DB_PASSWORD || "password",
-    {
-      dialect: process.env.DB_DIALECT || "mysql" || "sqlite",
-      host: process.env.DB_HOST,
-      storage: `./database.sqlite`,
-      logging: process.env.DB_LOGGING === "true",
-      pool: {
-        max: 2,
-        min: 0,
-      },
-      retry: {
-        max: 25,
-      },
-      dialectOptions: {
-        connectTimeout: 120000,
-      },
+  const dialect = process.env.DB_DIALECT || "mysql" || "sqlite"
+  if (dialect === 'mysql'){
+    const sequelize = new Sequelize(
+      process.env.DB_NAME || "note_synchronize",
+      process.env.DB_USERNAME || "root",
+      process.env.DB_PASSWORD || "password",
+      {
+        dialect,
+        host: process.env.DB_HOST,
+        storage: `./database.sqlite`,
+        logging: process.env.DB_LOGGING === "true",
+        pool: {
+          max: 2,
+          min: 0,
+        },
+        retry: {
+          max: 25,
+        },
+        dialectOptions: {
+          connectTimeout: 120000,
+        },
+      }
+    );
+    await initDatabase(sequelize, Object.keys(Models).map((modelName) => Models[modelName]));
+  } else {
+    // sqlite
+    // for scale, I leave each tables as a different database
+    const modelNames = Object.keys(Models);
+    for (let modelName of modelNames){
+      const sequelize = new Sequelize(
+        process.env.DB_NAME || "note_synchronize",
+        process.env.DB_USERNAME || "root",
+        process.env.DB_PASSWORD || "password",
+        {
+          dialect,
+          storage: `./database.${modelName}.sqlite`,
+          logging: process.env.DB_LOGGING === "true",
+          retry: {
+            max: 25,
+          },
+        }
+      );
+      await initDatabase(sequelize, [Models[modelName]]);
     }
-  );
-
-  const rawModels = Object.keys(Models).map((modelName) => Models[modelName]);
-
-  await initDatabase(sequelize, rawModels);
+  }
 };
