0923f85add1ff5fccd6f4abf51eb6da84e857eb6
Sy Le <lenguyensy@gmail.com>
2020-08-26 08:12:14 -0700

    now store the skipped and error message ids

--- server.ts
+++ server.ts
@@ -2,8 +2,9 @@
 import restify from "restify";
 import initDatabase from "./src/models/modelsFactory";
 import { initGoogleApi } from "./src/crawler/googleApiUtils";
-import * as gmailCrawler from "./src/crawler/gmailCrawler";
 import * as DataUtils from "./src/crawler/dataUtils";
+import * as gmailCrawler from "./src/crawler/gmailCrawler";
+import * as gdriveCrawler from "./src/crawler/gdriveCrawler";
 
 require("dotenv").config();
 
@@ -11,9 +12,7 @@ initDatabase();
 initGoogleApi();
 
 const server = restify.createServer();
-server.get("/api/message/:messageId", async function (req, res, next) {
-  let result = {};
-
+server.get("/api/message/parse/:messageId", async function (req, res, next) {
   const messageId = req.params.messageId;
 
   const email = await DataUtils.getEmailByMessageId(messageId);
@@ -21,8 +20,22 @@ server.get("/api/message/:messageId", async function (req, res, next) {
   try {
     res.send({
       raw: email.rawBody,
-      parsed_html: gmailCrawler._parseBodyWithHtml(email.rawBody),
-      parsed_text: gmailCrawler._parseBodyWithText(email.rawBody),
+      parsed_text: gmailCrawler.tryParseBody(email.rawBody),
+    });
+  } catch (error) {
+    res.send({ error });
+  }
+  next();
+});
+
+server.get("/api/message/sync/:messageId", async function (req, res, next) {
+  const messageId = req.params.messageId;
+
+  try {
+    await gdriveCrawler.uploadEmailMsgToGoogleDrive(messageId);
+
+    res.send({
+      ok: true,
     });
   } catch (error) {
     res.send({ error });
