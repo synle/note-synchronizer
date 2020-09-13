15364079bb1b49fa72df015391a1fb26e2b79c43
Sy Le <lenguyensy@gmail.com>
2020-09-12 19:49:50 -0700

    fixed test script for generate doc

--- test.ts
+++ test.ts
@@ -43,62 +43,59 @@ async function _doWork1() {
 async function _doWork2() {
 
   // const threadId = '10b81ba511e00280';
-  const threadId = "17214ac6b31840a3";
+  const threadId = "16ea8b8dddbbdf49";
 
   const emails = await DataUtils.getEmailsByThreadId(threadId);
 
-  const email = emails[0];
+  const sections = [];
+  const subject = emails[0].subject
+  for (let email of emails){
+    const attachments = await DataUtils.getAttachmentsByMessageId(email.id);
+    sections.push({
+      body: email.body,
+      images: attachments,
+    });
+  }
 
-  const attachments = await DataUtils.getAttachmentsByMessageId(email.id);
-
-  console.log(attachments[0].path);
-  console.log(attachments[0].fileName);
-  console.log(attachments[0].mimeType);
 
   try {
     const fileName = "./www.test.docx";
 
-    await gdriveCrawler.generateDocFile(
-      email.subject,
-      email.from,
-      email.rawBody,
-      attachments,
-      fileName
-    );
+    await gdriveCrawler.generateDocFile(subject, sections, fileName);
 
-    // first do a create operation
-    const resp1 = await googleApiUtils.uploadFile({
-      name: fileName,
-      mimeType: MIME_TYPE_ENUM.APP_MS_DOCX,
-      localPath: fileName,
-      description: "test html file 1",
-      date: email.date,
-      starred: false,
-      parentFolderId: "1c3KO8SOVv_era9g6uATFWGSG55Vhajf3",
-      appProperties: {
-        aaa: 111,
-        bbb: 222,
-      },
-    });
+    // // first do a create operation
+    // const resp1 = await googleApiUtils.uploadFile({
+    //   name: fileName,
+    //   mimeType: MIME_TYPE_ENUM.APP_MS_DOCX,
+    //   localPath: fileName,
+    //   description: "test html file 1",
+    //   date: email.date,
+    //   starred: false,
+    //   parentFolderId: "1c3KO8SOVv_era9g6uATFWGSG55Vhajf3",
+    //   appProperties: {
+    //     aaa: 111,
+    //     bbb: 222,
+    //   },
+    // });
 
-    const fileId = resp1.id;
-    console.log(fileId);
+    // const fileId = resp1.id;
+    // console.log(fileId);
 
-    // then do the update operation
-    await googleApiUtils.uploadFile({
-      name: fileName,
-      mimeType: MIME_TYPE_ENUM.APP_MS_DOCX,
-      localPath: fileName,
-      description: "test html file 2",
-      date: email.date,
-      starred: false,
-      parentFolderId: "1c3KO8SOVv_era9g6uATFWGSG55Vhajf3",
-      appProperties: {
-        aaa: 111,
-        bbb: 222,
-      },
-    });
-    console.log(resp1);
+    // // then do the update operation
+    // await googleApiUtils.uploadFile({
+    //   name: fileName,
+    //   mimeType: MIME_TYPE_ENUM.APP_MS_DOCX,
+    //   localPath: fileName,
+    //   description: "test html file 2",
+    //   date: email.date,
+    //   starred: false,
+    //   parentFolderId: "1c3KO8SOVv_era9g6uATFWGSG55Vhajf3",
+    //   appProperties: {
+    //     aaa: 111,
+    //     bbb: 222,
+    //   },
+    // });
+    // console.log(resp1);
   } catch (e) {
     console.log(e.stack);
   }
@@ -213,14 +210,13 @@ async function _doWork7() {
 async function _start() {
   await _init();
 
-  //
+  await _doWork2();
   //await _doWork0();
   // await _doWork6();
   //await _doWork3();
   //await _doWork4();
   //await _doWork7();
-
-  await _doWork7();
+  // await _doWork7();
   process.exit();
 }
 
